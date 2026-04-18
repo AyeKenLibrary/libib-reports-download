d823070a630b7df9fd8c67a5679c0da49e923743
AyeKenLibrary <ayekenlibrary@gmail.com>
2026-04-18 16:46:28 +0100

    Update fetch-libib.js

--- fetch-libib.js
+++ fetch-libib.js
@@ -129,7 +129,7 @@ async function run() {
  //const csvBuffer1 = fs.readFileSync(Path);
    // console.log(csvBuffer1);
 //await download1.saveAs("current-checkouts.csv");
-console.log(await page.locator('body').innerText());
+console.log(await page.content());
   const [download3] = await Promise.all([
   page.waitForEvent('download'),
   page.click('text=Export CSV')
