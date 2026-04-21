044a7c5664c62faf9102fa505615963f627dcdce
AyeKenLibrary <ayekenlibrary@gmail.com>
2026-04-21 13:23:27 +0100

    Update fetch-libib.js

--- fetch-libib.js
+++ fetch-libib.js
@@ -126,11 +126,12 @@ async function run() {
     //Wait for password form to appear
     await page.waitForSelector('input[type="password"]', { timeout: 15000 });
     console.log("Cloudflare Block Detection: ", await page.locator('script[src*="app"]').count());
-const realPassword = page.locator('input[type="password"]');
+const realPassword = page.locator('input[name="password"]');
 const realToken = page.locator('input[name="_token"]');
 
 const isRealLogin = await realPassword.count();
     console.log("Value of isRealLogin", isRealLogin);
+    throw e;
     //Enter password and submit
     await page.fill('input[type="password"]', LIBIB_PASSWORD);
     await page.click('button[type="submit"], input[type="submit"]');
